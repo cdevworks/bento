a56ff170504769634f0089abc5dfd04270ab59b2
Craig Tanaka <cdevworks@gmail.com>
2020-09-06 06:46:01 +0100

    target price

--- bento-www/src/bentoUtils/index.js
+++ bento-www/src/bentoUtils/index.js
@@ -118,7 +118,7 @@ export const getCurrentPrice = async (bento) => {
 }
 
 export const getTargetPrice = async (bento) => {
-  return bento.toBigN(1).toFixed(2);
+  return bento.toBigN(10).toFixed(20);
 }
 
 export const getCirculatingSupply = async (bento) => {
